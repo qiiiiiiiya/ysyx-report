0ee70c56fb5e6c3a0edd1a30aea2fed2378fa484
tracer-ysyx <tracer@ysyx.org>
2025-07-14 09:17:59 +0800

    > run NEMU 312309070403 杨雅淇 Linux yyq03-VMware-Virtual-Platform 6.11.0-21-generic #21~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 24 16:52:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:17:59 up 1 day, 23:20,  3 users,  load average: 0.55, 0.47, 0.75

---
+++ nemu/scripts/cpu-exec.c
@@ -0,0 +1,157 @@
+/***************************************************************************************
+* Copyright (c) 2014-2024 Zihao Yu, Nanjing University
+*
+* NEMU is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*          http://license.coscl.org.cn/MulanPSL2
+*
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+*
+* See the Mulan PSL v2 for more details.
+***************************************************************************************/
+
+#include <cpu/cpu.h>
+#include <cpu/decode.h>
+#include <cpu/difftest.h>
+#include <locale.h>
+
+/* The assembly code of instructions executed is only output to the screen
+ * when the number of instructions executed is less than this value.
+ * This is useful when you use the `si' command.
+ * You can modify this value as you want.
+ */
+#define MAX_INST_TO_PRINT 10//最多打印十条指令
+
+CPU_state cpu = {};//记录CPU的状态（执行到了哪个地址PC，寄存器值等）
+uint64_t g_nr_guest_inst = 0;//统计总共执行了多少条“guest”指令
+static uint64_t g_timer = 0; // unit: us//记录模拟器运行花费了多少时间（单位微秒）
+static bool g_print_step = false;//标记是否需要单步打印指令详情
+
+void device_update();
+
+static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
+#ifdef CONFIG_ITRACE_COND
+  if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
+#endif
+  if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
+  IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
+
+/*new*/
+#ifdef CONFIG_WATCHPOINT
+static void watchpoint_check(){
+  WP *cur=head;
+  while(cur){
+    bool success;
+    word_t new_val=expr(cur->expr,&success);
+    if(!success){
+      printf("表达式有误");
+      cur=cur->next;
+      continue;
+    }
+    if (new_val != cur->old_value) {  // 值变化：触发断点
+      printf("\nWatchpoint %d triggered:\n", cur->NO);
+      printf("Expression: %s\n", cur->expr);
+      printf("Old value: 0x%x\n", cur->old_value);
+      printf("New value: 0x%x\n", new_val);
+      cur->old_value = new_val;  // 更新旧值（下次基于新值检测）
+      nemu_state.state = NEMU_STOP;  // 暂停CPU执行
+      return;  // 找到一个变化即暂停
+    }
+    cur = cur->next;
+  }
+}
+#else
+#define watchpoint_check(addr) do {} while (0)
+#endif 
+/*new*/
+}
+
+static void exec_once(Decode *s, vaddr_t pc) {
+  s->pc = pc;
+  s->snpc = pc;
+  isa_exec_once(s);
+  cpu.pc = s->dnpc;
+#ifdef CONFIG_ITRACE
+  char *p = s->logbuf;
+  p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
+  int ilen = s->snpc - s->pc;
+  int i;
+  uint8_t *inst = (uint8_t *)&s->isa.inst;
+#ifdef CONFIG_ISA_x86
+  for (i = 0; i < ilen; i ++) {
+#else
+  for (i = ilen - 1; i >= 0; i --) {
+#endif
+    p += snprintf(p, 4, " %02x", inst[i]);
+  }
+  int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
+  int space_len = ilen_max - ilen;
+  if (space_len < 0) space_len = 0;
+  space_len = space_len * 3 + 1;
+  memset(p, ' ', space_len);
+  p += space_len;
+
+  void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
+  disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
+      MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst, ilen);
+#endif
+}
+
+static void execute(uint64_t n) {
+  Decode s;
+  for (;n > 0; n --) {
+    exec_once(&s, cpu.pc);
+    g_nr_guest_inst ++;
+    trace_and_difftest(&s, cpu.pc);
+    if (nemu_state.state != NEMU_RUNNING) break;
+    IFDEF(CONFIG_DEVICE, device_update());
+  }
+}
+
+static void statistic() {
+  IFNDEF(CONFIG_TARGET_AM, setlocale(LC_NUMERIC, ""));
+#define NUMBERIC_FMT MUXDEF(CONFIG_TARGET_AM, "%", "%'") PRIu64
+  Log("host time spent = " NUMBERIC_FMT " us", g_timer);
+  Log("total guest instructions = " NUMBERIC_FMT, g_nr_guest_inst);
+  if (g_timer > 0) Log("simulation frequency = " NUMBERIC_FMT " inst/s", g_nr_guest_inst * 1000000 / g_timer);
+  else Log("Finish running in less than 1 us and can not calculate the simulation frequency");
+}
+
+void assert_fail_msg() {
+  isa_reg_display();
+  statistic();
+}
+
+/* Simulate how the CPU works. */
+void cpu_exec(uint64_t n) {
+  g_print_step = (n < MAX_INST_TO_PRINT);
+  switch (nemu_state.state) {
+    case NEMU_END: case NEMU_ABORT: case NEMU_QUIT:
+      printf("Program execution has ended. To restart the program, exit NEMU and run again.\n");
+      return;
+    default: nemu_state.state = NEMU_RUNNING;
+  }
+
+  uint64_t timer_start = get_time();
+
+  execute(n);
+
+  uint64_t timer_end = get_time();
+  g_timer += timer_end - timer_start;
+
+  switch (nemu_state.state) {
+    case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
+
+    case NEMU_END: case NEMU_ABORT:
+      Log("nemu: %s at pc = " FMT_WORD,
+          (nemu_state.state == NEMU_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED) :
+           (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
+            ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
+          nemu_state.halt_pc);
+      // fall through
+    case NEMU_QUIT: statistic();
+  }
+}
