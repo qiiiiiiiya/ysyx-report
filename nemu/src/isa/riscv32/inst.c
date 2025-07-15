377fc9ed0166e3a103de1bd0b6b75af4eb10f8c3
tracer-ysyx <tracer@ysyx.org>
2025-07-15 14:17:32 +0800

    > run NEMU 312309070403 杨雅淇 Linux yyq03-VMware-Virtual-Platform 6.11.0-21-generic #21~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 24 16:52:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:17:32 up 2 days, 17:54,  3 users,  load average: 1.35, 0.68, 0.52

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -37,8 +37,8 @@ enum {
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
 
 //J型，
-#define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 20)  | (BITS(i, 21, 19) << 11) | (BITS(i, 19, 12)<<12)| (BITS(i, 30, 21) << 1) ; } while(0)
-//#define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | BITS(i, 30, 21) << 1 |BITS(i, 20, 20) << 11 | BITS(i, 19, 12) << 12 ; } while(0)
+//#define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 20)  | (BITS(i, 21, 19) << 11) | (BITS(i, 19, 12)<<12)| (BITS(i, 30, 21) << 1) ; } while(0)
+#define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | BITS(i, 30, 21) << 1 |BITS(i, 20, 20) << 11 | BITS(i, 19, 12) << 12 ; } while(0)
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst;
   int rs1 = BITS(i, 19, 15);
@@ -69,7 +69,8 @@ static int decode_exec(Decode *s) {
   //new
   INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi   , I, R(rd) = src1 + imm);
   INSTPAT("??????? ????? ????? ??? ????? 01101 11", lui    , U, R(rd) = imm << 12);
-  INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal    , J, R(rd) = s->snpc+4, s->dnpc = s->snpc + imm);
+  //INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal    , J, R(rd) = s->snpc+4, s->dnpc = s->snpc + imm);
+  INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal    , J, R(rd) = s -> pc + 4; s -> dnpc += imm -4;); // jal指
   //INSTPAT("0000000 ????? ????? 000 ????? 01100 11", li    , R(rd) = src1 + src2);
   INSTPAT("??????? ????? ????? 010 ????? 01000 11", sw     , S, Mw(src1 + imm, 4, src2));
   //new
